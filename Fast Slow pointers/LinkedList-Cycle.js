b0f94e000d6d4f09651e5b0ee14e49f48a03d650
jimweng <jimweng01@hotmail.com>
2021-03-04 21:34:37 +0800

    2021.3.4

---
+++ Fast Slow pointers/LinkedList-Cycle.js
@@ -0,0 +1,39 @@
+/**
+ *  想法：快慢pointers，就是在找是否有cycle
+ */
+
+class Node {
+    constructor(value, next=null){
+      this.value = value;
+      this.next = next;
+    }
+  }
+  
+  const has_cycle = function(head) {
+    let slow = head, fast = head;
+    
+    while (fast.next !== null && fast.next.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+  }
+  
+  
+  head = new Node(1)
+  head.next = new Node(2)
+  head.next.next = new Node(3)
+  head.next.next.next = new Node(4)
+  head.next.next.next.next = new Node(5)
+  head.next.next.next.next.next = new Node(6)
+  console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+  
+  head.next.next.next.next.next.next = head.next.next
+  console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+  
+  head.next.next.next.next.next.next = head.next.next.next
+  console.log(`LinkedList has cycle: ${has_cycle(head)}`)
