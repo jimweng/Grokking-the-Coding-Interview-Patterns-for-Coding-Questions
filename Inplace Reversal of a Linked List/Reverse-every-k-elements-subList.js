9eac5845b030e4793502b5c11ff96cfc33b6cdeb
jimweng <jimweng01@hotmail.com>
2021-05-19 10:55:56 +0800

    2021.5.15

---
+++ Inplace Reversal of a Linked List/Reverse-every-k-elements-subList.js
@@ -0,0 +1,75 @@
+/**
+ *  想法：
+ *      1. 多次sublist
+ * 
+ */
+
+class Node {
+    constructor(value, next=null){
+      this.value = value;
+      this.next = next;
+    }
+  
+    get_list() {
+      let result = "";
+      let temp = this;
+      while (temp !== null) {
+        result += temp.value + " ";
+        temp = temp.next;
+      }
+      return result;
+    }
+  };
+  
+  
+  
+  const reverse_every_k_elements = function(head, k) {
+    // edge case
+      if (k <= 1 || head == null) {
+          return head;
+      }
+
+    let current = head, previous = null;
+    while (true) {
+      const last_node_of_previous_part = previous;
+
+      const last_node_of_sub_list = current;
+      let next = null;
+      let i = 0;
+      console.log('i: ', i);
+      while (current !== null && i < k) {
+        next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+        i += 1;
+      }
+
+      if (last_node_of_previous_part !== null) {
+        last_node_of_previous_part.next = previous;
+      } else {
+        head = previous;
+      }
+
+      last_node_of_sub_list.next = current;
+
+      if (current === null) {
+        break;
+      }
+      previous = last_node_of_sub_list;
+    } 
+
+    return head;
+  }
+  
+  head = new Node(1)
+  head.next = new Node(2)
+  head.next.next = new Node(3)
+  head.next.next.next = new Node(4)
+  head.next.next.next.next = new Node(5)
+  head.next.next.next.next.next = new Node(6)
+  head.next.next.next.next.next.next = new Node(7)
+  head.next.next.next.next.next.next.next = new Node(8)
+  
+  console.log(`Nodes of original LinkedList are: ${head.get_list()}`)
+  console.log(`Nodes of reversed LinkedList are: ${reverse_every_k_elements(head, 3).get_list()}`)
